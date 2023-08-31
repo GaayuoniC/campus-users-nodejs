a0ab8688d72db99ef0612361927e418b24581d2d
Christian Gaayuoni <gayuoni@proton.me>
2023-08-31 12:03:53 +0200

    Add solution to last task

--- index4.js
+++ index4.js
@@ -2,8 +2,11 @@ import { users } from "./data.js";
 
 const onlyMaleUsers = users
   .filter((users) => users.gender === "male")
-  .map((users) => users.name);
+  .map(
+    (users) =>
+      users.name.first + " " + users.name.last + " is " + users.dob.age + " old"
+  );
 // ? male
 // : users.name.title + " " + users.name.first + " " + users.name.last
 
-console.log(onlyMaleUsers + " is " + users.male.age);
+console.log(onlyMaleUsers);
