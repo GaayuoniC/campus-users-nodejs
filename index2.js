433948867b8b379b2796adc06e7ba650bd523ba6
Christian Gaayuoni <gayuoni@proton.me>
2023-08-31 11:36:52 +0200

    Female criteria not working as expected

--- index2.js
+++ index2.js
@@ -1,7 +1,9 @@
 import { users } from "./data.js";
 
 const onlyFemales = users.map((users) =>
-  users.gender.female ? "female" : users.name.first + " " + users.name.last
+  users.gender.female 
+    ?  "female"
+    : users.name.title + " " + users.name.first + " " + users.name.last
 );
 
 console.log(onlyFemales);
