f44b8ba06a0f00dbf4603d0395dd8baf87efc1f2
Christian Gaayuoni <gayuoni@proton.me>
2023-08-31 11:07:19 +0200

    Add final solution for task 1

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { users } from "./data.js";
 
 const FirstLastNames = users.map(
-  (users) => users.name.first + " " + users.name.last 
+  (users) =>
+    users.name.first +
+    " " +
+    users.name.last +
+    " is from " +
+    users.location.country
 );
 
 console.log(FirstLastNames);
